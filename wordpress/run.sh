d8b39f2cbf1919dc9dd7a502cfbf2b104383602b
Kurt Gasser <office@gasser-partner.at>
2018-07-12 09:08:33 +0000

    code review run.sh

--- wordpress/run.sh
+++ wordpress/run.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
+# developed for TheDifferent by Florian Kleber for terms of use have a look at the LICENSE file
+
 # terminate on errors
 set -xe
 
+# define as docker compose var or default ""
+BACKUP_URL=${BACKUP_URL:-""}
+
 # check if volume is not empty
-if [ ! "$(ls -A "/var/www/wp-content" 2>/dev/null)" ]; then
-
-    # print info to cli
-    echo 'Setting up wp-content volume'
-
-    # copy wp-content from Wordpress src to volume
-    cp -r /usr/src/wordpress/wp-content /var/www/
-    chown -R nobody:nobody /var/www
-
+if [ ! $(ls -A "/var/www/wp-content" 2>/dev/null) ]; then
+    # check if BACKUP_URL exists by downloading the first byte
+    if [ "curl --output /dev/null --silent --fail -r 0-0 '$BACKUP_URL'" ]; then
+        # download backup from backup url
+        curl -sfL "$BACKUP_URL" | tar -zxC /var/www
+    else
+        # print info to cli
+        echo 'Setting up wp-content directory'
+        # copy wp-content from Wordpress src to directory
+        cp -r /usr/src/wordpress/wp-content /var/www/
+        chown -R nobody:nobody /var/www
+    fi
     # generate secrets
-    curl -f https://api.wordpress.org/secret-key/1.1/salt/ >> /usr/src/wordpress/wp-secrets.php
+    curl -sf https://api.wordpress.org/secret-key/1.1/salt/ >> /usr/src/wordpress/wp-secrets.php
 fi
 
 # execute CMD[]
