9403f3bb7b8ce607b0bb69f1248ad7689e6c4f92
kleberbaum <kleberbaum@erebos.xyz>
2018-08-06 01:18:45 +0200

    fixed boot behavior

--- wordpress/latest/run.sh
+++ wordpress/latest/run.sh
@@ -9,20 +9,20 @@ set -xe
 WP_BACKUP_URL=${WP_BACKUP_URL:-}
 
 # check if volume is not empty
-if [[ ! $(ls -A "/var/www/wp-content" 2>/dev/null) ]]; then
-	# check if BACKUP_URL exists by downloading the first byte
-	if [[ $(wget -S --spider $1  2>&1 | grep 'HTTP/1.1 200 OK') ]]; then
-		# download backup from backup url
-		wget -qO- "$WP_BACKUP_URL" | tar zxC /var/www
-	else
-		# print info to cli
-		echo 'Setting up wp-content directory'
-		# copy wp-content from Wordpress src to directory
-		cp -r /usr/src/wordpress/wp-content /var/www/
-		chown -R nobody:nobody /var/www
-	fi
-	# generate secrets
-	wget https://api.wordpress.org/secret-key/1.1/salt/ -O ->> /usr/src/wordpress/wp-secrets.php
+if [ ! -f /var/www/wp-content/index.php ]; then
+    # check if BACKUP_URL exists by downloading the first byte
+    if [[ $(wget -S --spider $1  2>&1 | grep 'HTTP/1.1 200 OK') ]]; then
+        # download backup from backup url
+        wget -qO- "$WP_BACKUP_URL" | tar zxC /var/www
+    else
+        echo 'Setting up wp-content directory'
+        # copy wp-content from Wordpress src to directory
+        cp -r /usr/src/wordpress/wp-content /var/www/
+    fi
+    # set owner to nobody
+    chown -R nobody:nobody /var/www
+    # generate wordpress secrets
+    wget https://api.wordpress.org/secret-key/1.1/salt/ -O ->> /usr/src/wordpress/wp-secrets.php
 fi
 
 # execute CMD[]
